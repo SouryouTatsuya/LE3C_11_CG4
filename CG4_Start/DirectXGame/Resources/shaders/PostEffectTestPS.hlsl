779cc553ed12e989e6fc4bd1b95eda2ecebabb46
SouryouTatsuya <k018g5020@g.neec.ac.jp>
2022-05-20 10:39:16 +0900

    単純なポストエフェクトで平均ぼかしを実装
    単純なポストエフェクトで平均ぼかしを実装

--- CG4_Start/DirectXGame/Resources/shaders/PostEffectTestPS.hlsl
+++ CG4_Start/DirectXGame/Resources/shaders/PostEffectTestPS.hlsl
@@ -5,6 +5,18 @@ SamplerState smp : register(s0);      // 0番スロットに設定されたサ�
 
 float4 main(VSOutput input) : SV_TARGET
 {
-	float4 texcolor = tex.Sample(smp, input.uv);
-	return float4(texcolor.rgb, 1);
+	//float4 texcolor = tex.Sample(smp, input.uv);
+	//return float4(texcolor.rgb, 1);
+	float u = 1.0f / 1280.0f;
+    float v = 1.0f / 720.0f;
+	float4 color = { 0,0,0,0 };
+	for (int i = 0; i < 11; i++)
+	{
+		for (int j = 0; j < 11; j++)
+		{
+			color += tex.Sample(smp, 
+				input.uv - float2(u * 5, v * 5) + float2(u * i, v * j));
+		}
+	}
+	return float4(color.rgb / (11 * 11), 1);
 }
