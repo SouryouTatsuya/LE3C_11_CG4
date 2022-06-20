2e1611bd017884051085c94a955d9cafad8f8ce0
SouryouTatsuya <103228951+SouryouTatsuya@users.noreply.github.com>
2022-06-20 14:08:02 +0900

    マテリアル用ルートパラメータ追加
    マテリアル用ルートパラメータ追加

--- CG4_Start/DirectXGame/Object3D.cpp
+++ CG4_Start/DirectXGame/Object3D.cpp
@@ -150,11 +150,13 @@ void Object3d::CreateGraphicsPipeline()
 	descRangeSRV.Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 0); // t0 レジスタ
 
 	// ルートパラメータ
-	CD3DX12_ROOT_PARAMETER rootparams[2];
+	CD3DX12_ROOT_PARAMETER rootparams[3];
 	// CBV（座標変換行列用）
 	rootparams[0].InitAsConstantBufferView(0, 0, D3D12_SHADER_VISIBILITY_ALL);
 	// SRV（テクスチャ）
 	rootparams[1].InitAsDescriptorTable(1, &descRangeSRV, D3D12_SHADER_VISIBILITY_ALL);
+	//CBV(マテリアル用)b1レジスタ
+	rootparams[2].InitAsConstantBufferView(1, 0, D3D12_SHADER_VISIBILITY_ALL);
 
 	// スタティックサンプラー
 	CD3DX12_STATIC_SAMPLER_DESC samplerDesc = CD3DX12_STATIC_SAMPLER_DESC(0);
