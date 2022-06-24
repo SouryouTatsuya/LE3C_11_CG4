4eb23dca39e6734060aa60eb45d8dc200c7f9311
SouryouTatsuya <103228951+SouryouTatsuya@users.noreply.github.com>
2022-06-24 10:17:00 +0900

    一時コミット
    一時コミット

--- CG4_Start/DirectXGame/Resources/shaders/FBXPS.hlsl
+++ CG4_Start/DirectXGame/Resources/shaders/FBXPS.hlsl
@@ -3,17 +3,51 @@
 Texture2D<float4> tex : register(t0);
 //0番目スロットに設定されたサンプラー
 SamplerState smp : register(s0);
+//π
+static const float PI = 3.141592654f;
+//反射点の法線ベクトル
+static float3 N;
+
+//双方向反射分布関数
+float3 BRDF(float3 L, float3 V)
+{
+	//法線とライト方向の内積
+	float NdotL = dot(N, L);
+	//法線とカメラ方向の内積
+	float NdotV = dot(N, V);
+	//どちらかが90度以上であれば真っ黒を返す
+	if (NdotL < 0 || NdotV < 0) { return float3(0, 0, 0); }
+
+	//拡散反射率
+	float diffuseReflectance = 1.0f / PI;
+	//拡散反射項
+	float3 diffuseColor = diffuseReflectance * NdotL * baseColor * (1 - metalness);
+
+	//ここに鏡面反射の式を挿入する
+
+	return diffuseColor;
+}
+
 //エントリーポイント
 float4 main(VSOutput input) : SV_TARGET
 {
-	//テクスチャマッピング
-	float4 texcolor = tex.Sample(smp, input.uv);
-	//Lambert反射
-	float3 light = normalize(float3(1, -1, 1)); //右下奥、向きのライト
-	float diffuse = saturate(dot(-light, input.normal));
-	float brightness = diffuse + 0.3f;
-	float4 shadercolor = float4(brightness, brightness, brightness, 1.0f);
-	//陰影とテクスチャの色を合成
-	//return shadercolor * texcolor;
-	return float4(ambientLightColor, 1);
+	//面の情報をstatic変数に代入して、関数から参照できるようにする
+	N = input.normal;
+    //最終的なRGB
+    float3 finalRGB = float3(0, 0, 0);
+	//頂点から視点へのお方向ベクトル
+	float3 eyedir = normalize(cameraPos - input.worldpos.xyz);
+
+	//平行光源
+	for (int i = 0; i < DIRLIGHT_NUM; i++)
+	{
+		if (!dirLights[i].active)
+		{
+			continue;
+		}
+		//BRDFの結果とライト色を合成
+		finalRGB *= BRDF(dirLights[i].lightv, eyedir) * dirLights[i].lightcolor;
+	}
+
+	return float4(finalRGB, 1);
 }
